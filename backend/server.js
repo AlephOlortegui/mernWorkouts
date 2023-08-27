75b886ab2ded66e516a68c73d19d18d95a12708b
NeoDev <you@example.com>
2023-08-26 23:38:12 -0500

    Initial project - just backend basics done - frontend just initial react template

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const workoutRoutes = require('./routes/workoutRoutes')
+const connectDB = require('./db/db')
+
+require('dotenv').config()
+// the config method it's going to attach those enviornment variables for us to the process object
+
+const app = express();
+
+connectDB(app)
+
+// sets up middleware for parsing JSON data in incoming HTTP requests.
+app.use(express.json())
+
+app.use((req,res,next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//app.use(myRoute, routesFile)
+app.use('/api/workouts', workoutRoutes)
+/* when we fire a request to 'myRoute' 
+  then I want to use workoutRoutes
+*/
