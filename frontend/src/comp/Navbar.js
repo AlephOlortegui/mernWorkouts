67ef75bb1675ff71c6c77b2c26359a8f50c8fb6b
NeoDev <you@example.com>
2023-08-28 22:31:04 -0500

    logout

--- frontend/src/comp/Navbar.js
+++ frontend/src/comp/Navbar.js
@@ -1,17 +1,26 @@
 import {Link} from 'react-router-dom'
+import useLogout from '../hooks/useLogout'
 
-const Navbar = () => (
+const Navbar = () => {
+  const {logout} = useLogout()
+
+  const handleClick = () => { logout() }
+
+  return(
     <header>
-        <div className="container">
-            <Link to="/"><h1>Workout Buddy</h1></Link>
-            <nav>
-                <div>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
-                </div>
-            </nav>
-        </div>
+      <div className="container">
+        <Link to="/"><h1>Workout Buddy</h1></Link>
+        <nav>
+          <div>
+           <button onClick={handleClick}>log out</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
+      </div>
     </header>
   )
-
+}
 export default Navbar
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -10,6 +10,8 @@ const AuthReducer = (state,action) => {
   switch (action.type) {
     case "LOGIN":
       return {user: action.payload}
+    case "LOGOUT":
+      return {user: null}
     default:
       return state
   }
