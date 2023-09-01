91e9c187772c52ed367ec151ba1273be24833de7
NeoDev <you@example.com>
2023-09-01 13:41:50 -0500

    protected routes if we are not loggen in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './comp/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route 
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
             <Route 
               path="/login" 
-              element={<Login />} 
+              element={!user ? <Login /> : <Navigate to="/" />} 
             />
             <Route 
               path="/signup" 
-              element={<Signup />} 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
             />
           </Routes>
         </div>
